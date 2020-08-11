f01584a93c3cf675ca73a26ac24ebb592a038d52
mark churcher <mark@developersinstitute.ac.nz>
2020-08-11 16:45:05 +1200

    tidy up

--- server/index.js
+++ server/index.js
@@ -1,22 +1,20 @@
 require('dotenv').config();
 const express = require("express");
-const app = express();
-const cors = require("cors");
+
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-var todoRouter = require('./routes/todo');
-
 const swaggerDoc = YAML.load('./openapi.yaml');
-console.log(swaggerDoc);
+
+
+const app = express();
+const cors = require("cors");
+var todoRouter = require('./routes/todo');
 
 //middleware
 app.use(cors());
 app.use(express.json()); //req.body
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-
-console.log(process.env);
 app.use('/todos', todoRouter);
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.listen(5000, () => {
   console.log("server has started on port 5000");
 });
